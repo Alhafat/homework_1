cb62e79a32fe9269b766f137991e01e4a7c0f254
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 20:34:41 +0300

    task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -7,11 +7,11 @@
 Console.Write("Введите первое число: ");
 int a=int.Parse(Console.ReadLine()!);
 
-Console.Write("Введите второе число: ");
-int b=int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int b=int.Parse(Console.ReadLine()!);
 
-Console.Write("Введите третье число: ");
-int c=int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число: ");
+// int c=int.Parse(Console.ReadLine()!);
 
 if (a%2==0)
 {
@@ -21,19 +21,19 @@ else
 {
     Console.WriteLine($"Число {a} является нечетным");
 }
-if (b%2==0)
-{
-    Console.WriteLine($"Число {b} является четным");
-}
-else
-{
-    Console.WriteLine($"Число {b} является нечетным");
-}
-if (c%2==0)
-{
-    Console.WriteLine($"Число {c} является четным");
-}
-else
-{
-    Console.WriteLine($"Число {c} является нечетным");
-}
+// if (b%2==0)
+// {
+//     Console.WriteLine($"Число {b} является четным");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {b} является нечетным");
+// }
+// if (c%2==0)
+// {
+//     Console.WriteLine($"Число {c} является четным");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {c} является нечетным");
+// }
