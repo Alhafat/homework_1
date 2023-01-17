8d3ad9691a65ce1deeca561f961cffce9ade687c
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 15:03:34 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число: ");
+// int a=int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число: ");
+// int b=int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите третье число: ");
+// int c=int.Parse(Console.ReadLine()!);
+
+while (true)
+{
+    Console.Write("Введите первое число: ");
+    int a=int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите второе число: ");
+    int b=int.Parse(Console.ReadLine()!);
+
+    if (a!=b)
+    {
+        Console.Write("Введите третье число: ");
+        int c=int.Parse(Console.ReadLine()!);
+    }
+    else
+    {
+        if ((b!=c)&(a!=c))
+            {
+                if ((a>b)&(a>c))
+                {
+                    Console.WriteLine($"Максимальное число {a}");
+                }
+                else
+                {
+                    if (b>c)
+                    {
+                        Console.WriteLine($"Максимальное число {b}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Максимальное число {c}");
+                    }
+                }
+            }
+        else
+        {
+            Console.WriteLine($"Введите неравные числа!!!");
+            continue;   
+        }
+    }
+break;
+}
